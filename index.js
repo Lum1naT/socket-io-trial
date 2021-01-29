66a8aa9b91ec6af0c9a09ba7cbdf03ba3321cbaf
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2021-01-29 15:08:37 +0100

    nameless chat

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 const router = express.Router();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+
+io.on('connection', (socket) => {
+  // on connection
+  console.log('a user connected');
+
+  // on disconnect
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+
+  });
+
+  
+});
 
 app.get('/', function(req, res) {
 
@@ -12,12 +33,13 @@ app.get('/', function(req, res) {
 
 });
 
+
 // use router
 app.use('/', router);
 
 // define your root for css and html files
 app.use(express.static(__dirname + '/src'));
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
