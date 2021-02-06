5acc79b37fa151b50630adaf99d59952f0f9581b
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2021-02-06 13:16:30 +0100

    unfinished, needs to be done in react

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cookie = require('cookie');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 const router = express.Router();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
+const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
 
+let loader = new TwingLoaderFilesystem('./src/templates');
+let twing = new TwingEnvironment(loader);
 // DO NOT EDIT
 var admin = require("firebase-admin");
 
@@ -22,7 +27,6 @@ admin.initializeApp({
 let database = admin.database();
 // -------
 
-var obj = {name: "username", value: "omega"};
 var usersRef = database.ref("users");
 var roomsRef = database.ref("rooms");
 var msgsRef = database.ref("messages");
@@ -46,15 +50,9 @@ io.on('connection', (socket) => {
   // on connection
   console.log('a user connected');
 
-usersRef.push(obj, function(error) {
-  if (error) {
-    // The write failed...
-    console.log("Failed with error: " + error)
-  } else {
-    // The write was successful...
-    console.log("success")
-  }
-});
+
+  var cookies = cookie.parse(socket.handshake.headers.cookie);    
+  console.log(cookies.userKey);
 
   // on disconnect
   socket.on('disconnect', () => {
@@ -63,7 +61,7 @@ usersRef.push(obj, function(error) {
 
 
   socket.on('chat message', (data) => {
-    console.log( data.username + ': ' + data.msg);
+    console.log( data.username + ': ' + data.msg + " > " + data.userKey);
     var date = new Date().toLocaleString("cs");
     io.emit('chat message', {username: data.username, msg: data.msg, timestamp: date});
 
@@ -72,29 +70,45 @@ usersRef.push(obj, function(error) {
   
 });
 
-app.get('/', function(req, res) {
-
-    res.sendFile(path.join(__dirname + '/src/templates/index.html'));
-
+app.get('/', function (req, res) {
+  twing.render('index.html.twig', {'name': 'World'}).then((output) => {
+      res.end(output);
+  });
 });
 
 app.post('/set-name', function(req, res) {
     var username = req.body.username;
-    res.cookie('username', username).redirect('/rooms');    // sets cookie username
+    var date = new Date().toLocaleString("cs");
+
+    var obj = {username: username, joined_at: date  };
+
+    var newUserRef = usersRef.push(obj, function(error) {
+      if (error) {
+        // The write failed...
+        console.log("Failed with error: " + error)
+      } else {
+        // The write was successful...
+        console.log("success")
+      }
+    });
+
+    var newUserKey = newUserRef.key;
+    res.cookie('userKey', newUserKey);
+    res.cookie('username', username);
+    res.redirect('/rooms');    // sets cookie userKey and username
 });
 
-app.get('/rooms', function(req, res){
 
-  res.sendFile(path.join(__dirname + '/src/templates/rooms.html'));
-
-
-
+app.get('/rooms', function (req, res) {
+  twing.render('rooms.html.twig', {'userKey': req.cookies.userKey, 'username': req.cookies.username}).then((output) => {
+      res.end(output);
+  });
 });
 
 
 // use router
 app.use('/', router);
-
+app.use(cors());
 
 
 // define your root for css and html files
